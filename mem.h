a5844dc5186a8503d5d1519702e078269cf6fa3e
calmopyrin <grosza@hotmail.com>
2017-09-14 23:52:31 +0200

    Memory handler classes added

--- mem.h
+++ mem.h
@@ -33,4 +33,52 @@ class DRVMEM : public MemoryHandler {
 		virtual void setNewRom(unsigned char *newRom) = 0;
 };
 
+class RomHandler : public MemoryHandler {
+private:
+protected:
+	unsigned char *mem;
+	unsigned int mask;
+	bool allocate;
+public:
+	RomHandler(const unsigned int size) : allocate(true) {
+		mem = new unsigned char[size];
+		mask = size - 1;
+	}
+	RomHandler(unsigned char *ram_, const unsigned int size) : allocate(false) {
+		mem = ram_;
+		mask = size - 1;
+	}
+	virtual ~RomHandler() {
+		if (allocate)
+			delete[] mem;
+	};
+	virtual void Write(unsigned int addr, unsigned char data) { };
+	virtual unsigned char Read(unsigned int addr) {
+		return mem[addr & mask];
+	}
+};
+
+class RamHandler : public RomHandler {
+private:
+protected:
+public:
+	virtual void Write(unsigned int addr, unsigned char data) {
+		mem[addr & mask] = data;
+	}
+};
+
+class CartMem : public MemoryHandler {
+private:
+	unsigned char *romBank[4];
+	unsigned int activeBank;
+protected:
+public:
+	CartMem(const unsigned int nofbanks) : activeBank(0) {
+		*romBank = new unsigned char[16384];
+	};
+	virtual ~CartMem() {
+		delete[] * romBank;
+	};
+};
+
 #endif // _MEM_H
